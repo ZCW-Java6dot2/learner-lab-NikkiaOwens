2849add46c161029d3e2b1cec51451393003970f
nikki <nikki@Gbrads-Mac.local>
2020-11-22 22:44:43 -0500

    Test Person

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,46 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+
+    @Test
+    public void testSetName(){
+        //Given
+        Person person = new Person();
+        String expected = "Nikkia";
+
+        //When
+        person.setName(expected);
+        String actual = person.getName();
+        //Then
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void testConstructor(){
+        //Given
+        String expected = "Owens";
+
+        //When
+        Person person = new Person(expected);
+        String actual = person.getName();
+
+        //Then
+
+        Assert.assertEquals(expected, actual);
+    }
+
+//    //Given
+//    String[] numberArray = {"1", "5", "15", "9", "4"};
+//    String[] expected = {"1", "4", "5", "9", "15"};
+//    //When
+//        flower.sort(numberArray);
+//
+//    //Then
+//    assertArrayEquals(expected, numberArray);
 
 }
