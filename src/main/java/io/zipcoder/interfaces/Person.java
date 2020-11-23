80930ad3df58c756915ff890287ebe014c150721
nikki <nikki@Gbrads-Mac.local>
2020-11-22 22:37:11 -0500

    1.1 Create Person Class

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -2,4 +2,16 @@ package io.zipcoder.interfaces;
 
 public class Person {
 
+    String name;
+    public Person(String name){
+        this.name = name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
